7704f04d40b99da4190b795703db7c89448e9c05
yoske <yoske95@gmail.com>
2016-04-18 16:07:10 +0900

    initial commit(not tested)

---
+++ public/client.js
@@ -0,0 +1,124 @@
+/* Goita Client Class
+   handles WebSocket messages */
+var MsgClient = function(){
+  //private field
+  this._eventDefined = false;
+
+  //member field
+  this.socket = null;  //socket.io
+  this.serverURI = null;
+  this.isConnected = false;
+  this.isAuthorized = false;
+
+  this.userId = null;
+
+  //event - inject event handler
+  this.connected = fnEmpty; //function()
+  this.connectFailed = fnEmpty;//function()
+  this.disconnected = fnEmpty; //function()
+  this.loginSuccess = fnEmpty;
+  this.loginFailed = fnEmpty;
+  this.MessageRecieved = fnEmpty; //function(msg [, header[, type]])
+
+};
+
+//class method
+MsgClient.prototype = {
+  
+  //connect to server
+  connect : function(){
+    //already isConnected
+    if(this.isConnected) return this.socket;
+
+    var self = this;  //capture a client instance
+
+    var socket;
+    if(this.serverURI != null)
+      io.connect(this.serverURI);
+    else
+      socket = io.connect();
+    this.socket = socket;
+    
+    //socketが無事取得できていればこの時点で接続確立しているはず。
+    if(socket != undefined && socket != null){
+      console.log("got socket-client successfully");
+    }
+    else{
+      this.isConnected = false;
+      this.connectFailed();
+    }
+
+    //for reconnecting, no need to define events again
+    if(this._eventDefined) return socket;
+
+    //------------define events ------------------
+    
+    // 接続できたというメッセージを受け取ったら
+    socket.on("connect", function() {
+      self.isConnected = true;
+      console.log("client connected!");
+      self.connected();
+    });
+    
+    //切断した場合
+    socket.on('disconnect', function(){
+      self.isConnected = false;
+      self.userId = null;
+      self.isAuthorized = false;
+      console.log("client disconnected");
+      self.disconnected();
+    });
+
+    //unhandled error
+    socket.on("error", function(error){
+      console.log("happened error: " + error);
+    });
+
+    // ロビーに入ったというメッセージを受け取ったら
+    socket.on("password accepted", function(data){
+      console.log("password accepted");
+      socket.id = data.id; //特に使う場面がないが一応
+      self.userId = data.id;
+      self.isAuthorized = true;
+      self.loginSuccess();
+    });
+
+    //ロビーに入れなかった場合
+    socket.on("password denied", function(){
+      console.log("failed to log in");
+      self.loginFailed(error);
+    });
+
+
+
+    //ロビーメッセージを受け取ったら msg={text: message text, username: user name}
+    socket.on("push msg", function(msg) {
+      self.MessageRecieved(msg);
+    });
+
+    //to avoid overloading event
+    this._eventDefined = true;
+    return socket;
+  },
+
+  //close connection
+  disconnect : function(){
+    this.socket.close();
+    console.log("client disconnected...");
+  },
+
+  login : function(pass){
+    this.socket.emit("password", pass);
+  },
+
+  send : function(json){
+    this.socket.emit("send msg", json);
+  }
+};
+
+
+//empty method
+var fnEmpty = function(){
+  var callerName = fnEmpty.caller != null ? fnEmpty.caller.name : "root";
+  console.log("unhandled event raised, caller name:" + callerName); 
+};
---
+++ server.js
@@ -0,0 +1,71 @@
+var pass_phrase = "inputyourpasswordhere";
+
+var validator= require('validator');
+
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+//WebSocketサーバーの定義
+io.set('transports', ['websocket', 'polling']); //websocketsに限定する場合に指定。c9.ioではコメントアウト
+//io.set('match origin protocol', true);
+//io.set("log level", 1);
+
+server.listen(port, function () {
+  console.log('Version: ' + process.version);
+  console.log('Server listening at port %d', port);
+});
+
+process.on('exit', function(){
+  server.close();
+});
+process.on('uncaughtException', function(){
+  server.close();
+});
+process.on('SIGTERM', function(){
+  server.close();
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// パスフレーズのみの簡易認証のためのユーザリスト
+var userList = {}; //{id : bool}
+
+//bind message func to individual socket
+io.sockets.on("connection", function(socket) {
+
+  var disconnected = function () {
+    
+    // delete from userList
+    delete userList[socket.id];
+    
+    console.log("user: ", Object.keys(userList).length);
+  };
+  
+  // 接続が途切れたときのイベントリスナを定義
+  socket.on("disconnect", disconnected);
+
+  socket.on("password", function (pass){
+    if(pass && pass == pass_phrase){
+      socket.emit("password accepted", {id:socket.id});
+      userList[socket.id] = true;
+      return;
+    }
+    
+    socket.emit("password denied");    
+  });
+
+  // クライアントからのメッセージ送信を受け取ったとき
+  socket.on("send msg", function(msg) {
+    if(socket.id in userList && msg)
+    {
+      socket.broadcast.emit("push msg", msg);
+    }
+  });
+});
+
+console.log('server started');
